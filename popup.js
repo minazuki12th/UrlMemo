08dcc366d42e26bec34915a5a002e3fc0d1a9340
minazuki12th <minazuki12th@gmail.com>
2013-12-09 22:14:20 +0900

    初回追加

---
+++ popup.js
@@ -0,0 +1,72 @@
+﻿$(function() {
+	$('#submit-ok').click(function() {
+		var title = $('#title-input').val();
+		if(title == undefined || title == ""){
+			alert("タイトルを入力してください");
+		}
+		else{
+			var url;
+			chrome.tabs.getSelected(null,function(tab) {
+			  	urlStorage.setUrlMemo(title, tab.url);
+			  	
+			  	$('#title-input').val("");
+				drawUrlList(urlStorage.getUrlList());
+			});
+  		}
+	});
+	
+	$('#submit-cancel').click(function() {
+		window.close();
+	});
+	
+	chrome.tabs.getSelected(null,function(tab) {
+	  	$('#title-input').val(tab.url);
+	  	$('#tab-url').text(tab.url);
+	});
+	
+  	drawUrlList(urlStorage.getUrlList());
+  	
+	function drawUrlList(urlList){
+		$('#urllist').empty();
+		
+		var i;
+		for(i = urlList.length - 1; i >= 0; i--){
+			listitem = CreateListItem(urlList[i], i);
+			$('#urllist').append(listitem)
+		}
+	}
+	
+	function CreateListItem(urlitem, itemIndex){
+		var listitem = $('<li>');
+		var linkelm = $('<a>');
+		linkelm.attr('href', urlitem.url);
+		linkelm.text(urlitem.title);
+		linkelm.click(function(){
+			chrome.tabs.create(
+				{url: urlitem.url},
+			    function(tab) {
+			        window.close(); 
+			    }
+			);
+		});
+		
+		var delelm = $('<a>');
+		delelm.attr('href', "#");
+		delelm.text("delete");
+		delelm.click(function(){
+			if(confirm("削除しますか？")){
+				urlStorage.removeUrlMemo(itemIndex);
+				drawUrlList(urlStorage.getUrlList());
+			}
+		});
+		
+		listitem.append(linkelm);
+		listitem.append(" [");
+		listitem.append(delelm);
+		listitem.append("]");
+		
+		return listitem;
+	}
+});
+
+
---
+++ stragemanager.js
@@ -0,0 +1,45 @@
+﻿var storageManager = (function () {
+
+    var my = {};
+
+    my.get = function (key) {
+        return localStorage.getItem(key);
+    }
+    my.put = function (key, value) {
+        return localStorage.setItem(key, value);
+    }
+    my.delete = function (key) {
+        return localStorage.removeItem(key);
+    }
+    
+    return my;
+
+}());
+
+var urlStorage = (function (storage) {
+    var my = {};
+    
+    var urlKey = "urlmemolist";
+    
+    if( !storage.get(urlKey) ){
+    	storage.put(urlKey, JSON.stringify([]));
+    }
+    
+    my.getUrlList = function(){
+    	return JSON.parse(storage.get(urlKey));
+    };
+    
+    my.setUrlMemo = function(title, url){
+    	var urlList = JSON.parse(storage.get(urlKey));
+    	urlList.push({title: title, url: url});
+    	storage.put(urlKey, JSON.stringify(urlList));
+    };
+    
+    my.removeUrlMemo = function(index){
+    	var urlList = JSON.parse(storage.get(urlKey));
+    	urlList.splice(index, 1);
+    	storage.put(urlKey, JSON.stringify(urlList));
+    };
+    
+    return my;
+}(storageManager));
